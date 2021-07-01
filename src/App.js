3c1ccfb2a6df8d8f765a467197c35f408f251f42
Marius <marius.berg.lindegaard@gmail.com>
2021-07-01 03:13:52 +0200

    Initial push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  document.title = 'ColosseumRP'
+
+  return (
+    <div className="menyBar">
+      <h1 className="menyTitle">Colosseum</h1>
+    </div>
+  );
+}
+
+export default App;
